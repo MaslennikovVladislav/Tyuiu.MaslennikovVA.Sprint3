839ab2be9938342d5bddb150d7bfc17655ebb548
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-27 20:48:58 +0500

    Task0

---
+++ Tyuiu.MaslennikovVA.Sprint3.Task0.V26.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MaslennikovVA.Sprint3.Task0.V26.Lib
+{
+    public class DataService : ISprint3Task0V26
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double multiSeries = 1;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+            {
+                multiSeries = multiSeries * (Math.Pow(2, i) / (i + 1)) * Math.Cos(1.8);
+            }
+            return Math.Round(multiSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.MaslennikovVA.Sprint3.Task0.V26/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MaslennikovVA.Sprint3.Task0.V26.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint3.Task0.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Масленников В.А. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Масленников Владислав Александрович | ИИПб-24-2               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение   *");
+            Console.WriteLine("* ряда по формуле.                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 1;
+            int stopValue = 9;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(startValue, stopValue));
+
+            Console.ReadKey();
+
+        }
+    }
+}
