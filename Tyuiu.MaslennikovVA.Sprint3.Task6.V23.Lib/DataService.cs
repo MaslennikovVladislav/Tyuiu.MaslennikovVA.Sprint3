5d2362eb2dea7447a47af6c8df88b115eb3c5e4a
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-27 21:26:48 +0500

    Task6

---
+++ Tyuiu.MaslennikovVA.Sprint3.Task6.V23.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MaslennikovVA.Sprint3.Task6.V23.Lib
+{
+    public class DataService : ISprint3Task6V23
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int x;
+            int sum = 0;
+            for (x = startValue; x <= stopValue; x++)
+            {
+                for (int d = 1; d <= x; d++)
+                {
+                    if (d % x == 0 && x < 11)
+                    {
+                        sum += d;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.MaslennikovVA.Sprint3.Task6.V23.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.MaslennikovVA.Sprint3.Task6.V23.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint3.Task6.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 10;
+            int stopValue = 15;
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 10;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
