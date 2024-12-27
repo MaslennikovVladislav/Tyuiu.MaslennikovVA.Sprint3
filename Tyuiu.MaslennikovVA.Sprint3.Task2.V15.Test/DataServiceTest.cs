898bb6ca088344e84a5464bb1366a5b8828841b6
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-27 21:11:25 +0500

    Task2

---
+++ Tyuiu.MaslennikovVA.Sprint3.Task2.V15.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.MaslennikovVA.Sprint3.Task2.V15.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint3.Task2.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 20;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+
+            double wait = 0.028;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
