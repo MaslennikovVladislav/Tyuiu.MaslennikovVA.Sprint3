abf39c95586c8f97775ba5262602e6e884e1b275
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-27 21:14:19 +0500

    Task3

---
+++ Tyuiu.MaslennikovVA.Sprint3.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.MaslennikovVA.Sprint3.Task3.V1.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint3.Task3.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCharCount()
+        {
+            DataService ds = new DataService();
+            string str = "have a nice time";
+            char chr = 'a';
+
+            double res = ds.GetCharCount(str, chr);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.MaslennikovVA.Sprint3.Task3.V1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MaslennikovVA.Sprint3.Task3.V1.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint3.Task3.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Масленников В.А. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Масленников Владислав Александрович | ИИПб-24-2               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach подсчитать количество букв “а” в строке.         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "have a nice time";
+            char chr = 'a';
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("искомый символ = " + chr);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Колличество символов = " + ds.GetCharCount(value, chr));
+
+            Console.ReadKey();
+
+        }
+    }
+}
